930f141ec673ebe9494ca3dc1da80a8e12ca1167
ypcrts <32625281+ypcrts@users.noreply.github.com>
2016-11-24 17:04:44 -0500

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='envr',
     packages=['envr'],
-    version='0.2.1',
+    version='0.3.0',
     description=('Manipulate and transform .env files ' +
                  'that are a subset of POSIX-compliant shell scripts.'),
     install_requires=[
